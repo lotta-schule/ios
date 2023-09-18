81c40a073c9a2484c98a4dc61295344df9bfcf3e
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2023-09-18 15:59:37 +0200

    Initial Commit

---
+++ lotta/lottaApp.swift
@@ -0,0 +1,32 @@
+//
+//  lottaApp.swift
+//  lotta
+//
+//  Created by Alexis Rinaldoni on 18/09/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct lottaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

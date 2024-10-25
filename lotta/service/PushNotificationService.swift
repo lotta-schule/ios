1edbec941e0bcad02cb15c786fafc77503341b6e
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2024-10-25 21:17:03 +0200

    Always init ModelData

--- lotta/service/PushNotificationService.swift
+++ lotta/service/PushNotificationService.swift
@@ -40,80 +40,93 @@ class PushNotificationService: NSObject, UNUserNotificationCenterDelegate {
     }
     
     func didReceiveRemoteNotification(userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        print("Did receive remote notification: \(userInfo)")
-        guard let tenantId = userInfo["tenant_id"] as? Int else {
-            SentrySDK.capture(message: "Received push notification without tenant_id")
-            completionHandler(.failed)
-            return
-        }
-        guard let session = ModelData.shared.userSessions.first(where: { $0.tenant.id == tenantId.formatted(.number) }) else {
-            SentrySDK.capture(message: "Received push notification for unknown tenant \(tenantId)")
-            completionHandler(.failed)
-            return
-        }
-        switch UIApplication.shared.applicationState {
-        case .active:
-            Task {
-                do {
-                    try await session.loadConversations(force: true)
-                    completionHandler(.newData)
-                } catch {
-                    print("Error refetching conversations: \(error)")
-                    completionHandler(.failed)
-                }
+        let appState = UIApplication.shared.applicationState
+        Task {
+            if !ModelData.shared.initialized {
+                await ModelData.shared.initializeSessions()
             }
-            
-        case .background:
-            guard let conversationId = userInfo["conversation_id"] as? String else {
-                SentrySDK.capture(message: "Received push notification for unknown conversation \(tenantId)")
+            let crumb = Breadcrumb(level: .info, category: "PushNotification")
+            crumb.message = "Received push notification: \(userInfo)"
+            SentrySDK.addBreadcrumb(crumb)
+            guard let tenantId = userInfo["tenant_id"] as? Int else {
+                SentrySDK.capture(message: "Received push notification without tenant_id")
                 completionHandler(.failed)
                 return
             }
-            session.api.apollo.fetch(query: GetConversationQuery(id: conversationId, markAsRead: false), cachePolicy: .fetchIgnoringCacheData) { result in
-                switch result {
-                case .success(let graphqlResult):
-                    if let conversationData = graphqlResult.data?.conversation {
-                        session.api.apollo.store.withinReadWriteTransaction { transaction in
-                            // add / update the conversation to the conversations list
-                            let getConversationsQueryCache = try transaction.read(query: GetConversationsQuery())
-                            let addConversationCacheMutation = AddConversationLocalCacheMutation()
-                            
-                            try transaction.update(addConversationCacheMutation) { (data: inout AddConversationLocalCacheMutation.Data) in
-                                let newConversation = AddConversationLocalCacheMutation.Data.Conversation(
-                                    _fieldData: conversationData._fieldData
-                                )
-                                if let i = getConversationsQueryCache.conversations?.firstIndex(where: { $0?.id == conversationId }) {
-                                    // conversation already is in our cache. Just update with the new message
-                                    data.conversations?[i]?.updatedAt = newConversation.updatedAt
-                                    data.conversations?[i]?.unreadMessages = newConversation.unreadMessages
-                                } else {
-                                    // conversation is new, add it to the cache
-                                    data.conversations?.append(newConversation)
-                                    // newConversation.messages = [AddConversationLocalCacheMutation.Data.Conversation.Message(id: messageId)]
+            guard let session = ModelData.shared.userSessions.first(where: { $0.tenant.id == tenantId.formatted(.number) }) else {
+                SentrySDK.capture(message: "Received push notification for unknown tenant \(tenantId)")
+                completionHandler(.failed)
+                return
+            }
+            switch appState {
+            case .active:
+                let crumb = Breadcrumb(level: .info, category: "PushNotification")
+                crumb.message = "Application is active"
+                SentrySDK.addBreadcrumb(crumb)
+                Task {
+                    do {
+                        try await session.loadConversations(force: true)
+                        completionHandler(.newData)
+                    } catch {
+                        SentrySDK.capture(error: error)
+                        completionHandler(.failed)
+                    }
+                }
+                
+            case .background:
+                let crumb = Breadcrumb(level: .info, category: "PushNotification")
+                crumb.message = "Application is in background"
+                SentrySDK.addBreadcrumb(crumb)
+                guard let conversationId = userInfo["conversation_id"] as? String else {
+                    SentrySDK.capture(message: "Received push notification for unknown conversation \(tenantId)")
+                    completionHandler(.failed)
+                    return
+                }
+                session.api.apollo.fetch(query: GetConversationQuery(id: conversationId, markAsRead: false), cachePolicy: .fetchIgnoringCacheData) { result in
+                    switch result {
+                    case .success(let graphqlResult):
+                        if let conversationData = graphqlResult.data?.conversation {
+                            session.api.apollo.store.withinReadWriteTransaction { transaction in
+                                // add / update the conversation to the conversations list
+                                let getConversationsQueryCache = try transaction.read(query: GetConversationsQuery())
+                                let addConversationCacheMutation = AddConversationLocalCacheMutation()
+                                
+                                try transaction.update(addConversationCacheMutation) { (data: inout AddConversationLocalCacheMutation.Data) in
+                                    let newConversation = AddConversationLocalCacheMutation.Data.Conversation(
+                                        _fieldData: conversationData._fieldData
+                                    )
+                                    if let i = getConversationsQueryCache.conversations?.firstIndex(where: { $0?.id == conversationId }) {
+                                        // conversation already is in our cache. Just update with the new message
+                                        data.conversations?[i]?.updatedAt = newConversation.updatedAt
+                                        data.conversations?[i]?.unreadMessages = newConversation.unreadMessages
+                                    } else {
+                                        // conversation is new, add it to the cache
+                                        data.conversations?.append(newConversation)
+                                        // newConversation.messages = [AddConversationLocalCacheMutation.Data.Conversation.Message(id: messageId)]
+                                    }
+                                }
+                                Task {
+                                    await ModelData.shared.setApplicationBadgeNumber()
+                                    completionHandler(.newData)
                                 }
                             }
-                            Task {
-                                await ModelData.shared.setApplicationBadgeNumber()
-                                completionHandler(.newData)
-                            }
                         }
+                    case .failure(let error):
+                        SentrySDK.capture(error: error)
+                        print(error)
+                        completionHandler(.failed)
                     }
-                case .failure(let error):
-                    SentrySDK.capture(error: error)
-                    print(error)
-                    completionHandler(.failed)
                 }
+            default:
+                print("notification: \(userInfo)")
+                completionHandler(.noData)
             }
-        default:
-            print("notification: \(userInfo)")
-            completionHandler(.noData)
         }
     }
     
     private func requestPermission() -> Void {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
             if let error = error {
-                print("D'oh: \(error.localizedDescription)")
                 SentrySDK.capture(error: error)
             } else {
                 UIApplication.shared.registerForRemoteNotifications()
@@ -121,11 +134,13 @@ class PushNotificationService: NSObject, UNUserNotificationCenterDelegate {
         }
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                didReceive response: UNNotificationResponse,
-                withCompletionHandler completionHandler:
-                   @escaping () -> Void) {
-       print(response)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        let crumb = Breadcrumb(level: .info, category: "PushNotication")
+        crumb.message = "Received push notification"
+        crumb.data = response.notification.request.content.userInfo as? [String: Any]
+        if !ModelData.shared.initialized {
+            await ModelData.shared.initializeSessions()
+        }
         switch response.actionIdentifier {
             case UNNotificationDefaultActionIdentifier:
             let threadIdentifier = response.notification.request.content.threadIdentifier.split(separator: "/")
@@ -137,8 +152,6 @@ class PushNotificationService: NSObject, UNUserNotificationCenterDelegate {
             default:
                 print("Unknown action \(response.actionIdentifier)")
         }
-       
-       completionHandler()
     }
     
 }
